ea38c0b3991cec4bfb0f68ae652f4c26bdb8ac01
kroppheather <kroppheather@gmail.com>
2018-05-09 14:38:09 -0400

    updated comments

--- appendix/mult_comp_example_code.r
+++ appendix/mult_comp_example_code.r
@@ -6,51 +6,53 @@
 model{
   # hierarhical model (focal model)
   for(i in 1:Nobs){
-	#likelihood for the data for all groups 
+	#likelihood for the data for all group levels
     Y[i]~dnorm(mu[groupID[i]],tau)
-	#log likelihood
+	#log likelihood for each observation
     loglike[i]<-.5*log(tau/(2*3.141593))-((tau/2)*pow(Y[i]-mu[groupID[i]],2))
   }
-  #assign noninformative prior to group means and variance
+  #assign noninformative hierarchical prior to the group-level means 
   for(i in 1:Ngroup){
       mu[i]~dnorm(m,tau.s)
 
     }
 
-    #assign noninformative prior for mean
+    #assign noninformative prior for mean to global mean
     m~dnorm(0,0.0001)
-	#non informative variance priors
+	#non-informative variance priors to the standard deviation terms
     tau<-pow(sig,-2)
     sig~dunif(0,100)
 	tau.s<-pow(sig.s,-2)
     sig.s~dunif(0,100)
-	#sum log likelihood
-    sumLL<-sum(loglike[1:Nobs])
+
   
   
   
-  #non-hierichical version
+  #non-hierarchical version
   for(i in 1:Nobs){
     Y.nh[i]~dnorm(mu.nh[groupID[i]],tau.nh)
 	#log likelihood
     loglike.nh[i]<-.5*log(tau.nh/(2*3.141593))-((tau.nh/2)*pow(Y.nh[i]-mu.nh[groupID[i]],2))
   }
-  #assign noninformative prior
+  #assign independent noninformative prior to group-level means
   for(i in 1:Ngroup){
       mu.nh[i]~dnorm(0,0.0001)
 
     }
-	
+	#assign non-informative prior to standard deviation:	
     tau.nh<-pow(sig.nh,-2)
     sig.nh~dunif(0,100)
-    sumLL.nh<-sum(loglike.nh[1:Nobs])
+
   
   #calculate pairwise difference
-  #between group means
+  #between group-level means Ncomp is the number of pairwise
+	# comparisons, and g.c1 and g.c2 are indexing variables that denote the
+	# group-level id’s associate with the first and second mean in the comparison
+
   for(i in 1:Ncomp){
 	#hierarchical pairwise difference
     diff[i]<-mu[g.c1[i]]-mu[g.c2[i]]
-	#non-hierarchical pairwise difference
+	#non-hierarchical pairwise difference (likely not relevant or reported)
     diff.nh[i]<-mu.nh[g.c1[i]]-mu.nh[g.c2[i]]
   }	
   #complete pooling 
@@ -59,12 +61,12 @@ model{
 	#log likelihood
     loglike.cp[i]<-.5*log(tau.cp/(2*3.141593))-((tau.cp/2)*pow(Y.cp[i]-mu.cp,2))
   }
-  #assign noninformative prior
+  #assign noninformative priors to global mean and standard deviation:
 
     mu.cp~dnorm(0,.0001)
     tau.cp<-pow(sig.cp,-2)
     sig.cp~dunif(0,100)
-    sumLL.cp<-sum(loglike.cp[1:Nobs])
+
   
 }
 
--- appendix/mult_comp_example_script_appendix.r
+++ appendix/mult_comp_example_script_appendix.r
@@ -1,5 +1,6 @@
-#######################################################################
-####### Script to run example multiple comparisions simulation ########
+########################################################################
+####### Script to implemen Bayesian models that analyze          ####### 
+####### pseudo data and computing the associated PPI             #######
 ########################################################################
 
 #load jags libraries
@@ -30,48 +31,47 @@ wd.data <- paste0(local.path,"\\appendix_data")
 # read in data files            #
 #################################
 #read in all data
-#randomly generated data used in the simulations
+#pseudo data for a single simulation replicate, for a subset of scenarios
 datMT <- read.csv(paste0(wd.data,"\\randomDataAppendix.csv"))
-#description of each simulation scenario
+#description of each scenario
 scnData <- read.csv(paste0(wd.data,"\\scenarioDataAppendix.csv"))
-#true means for comparision of groups in each scenario
+#true means for pairwise comparisons among groups within each scenario
 groupCompD <-  read.csv(paste0(wd.data,"\\groupComparisionDataAppendix.csv"))
 
 
 #################################
-# organize data for             #
-# example simulations           #
+# organize psuedo data for      #
+# example scenarios             #
 #################################
 
-#this appendix focuses on a subset of 4 simulations
-#that are examples of simulations with 
+#this appendix focuses on a subset of 4 pseudo datasets with:
 
-#scenario identifier for example simulation
+#scenario identifier for example pseudo dataset
 simN <- scnData$scenarioid
 
 #number of simulations that we will look at (here its 4)
-numsims<-length(simN)
+Ndatasets<-length(simN)
 
 #subset the data for simulation so that each simulation
 #can be run separately
 ranD <- list()
 groupD <- list()
-for(i in 1:numsims){
+for(i in 1:Ndatasets){
 	ranD[[i]] <- datMT[datMT$scenarioid==simN[i],]
 	groupD[[i]] <- groupCompD[groupCompD$scenarioid==simN[i],]
 }
 
 
 #################################
-# Set up model run ##############
+# Set up Bayesian model run #####
 #################################
 
-#run the model for each simulation scenario
+#run the Bayesian models for each scenario. Implement 3 model variants:
 #hierarchical, non-hierarchical, and complete pooling models
 #are all run in the same model code for each simulation
 
-#set up model runs for each scenario to test
-for(i in 1:numsims){
+#set up model runs for each dataset to test
+for(i in 1:Ndatasets){
 	datalist <- list(Nobs=dim(ranD[[i]])[1],Y=ranD[[i]]$Y, groupID=ranD[[i]]$groupid,
 						Y.nh=ranD[[i]]$Y.nh,Y.cp=ranD[[i]]$Y.cp,
 						Ngroup=scnData$grps[i], Ncomp=dim(groupD[[i]])[1],
@@ -84,13 +84,13 @@ for(i in 1:numsims){
 					  data=datalist,
 					  n.adapt=2000,
 					  n.chains=3)
-	#update the initialized Bayesian (JAGS) models and monitor quantities (parameters) of interest
+	#update the initialized Bayesian models and monitor quantities (parameters) of interest via JAGS
 	 s.mod.coda=coda.samples(s.mod.init,variable.names=c("mu","m","sig.s","sig","mu.nh","sig.nh",
 											  "mu.cp","sig.cp","sumLL.cp","sumLL.nh",
 											  "sumLL","diff","diff.nh", "loglike","loglike.nh","loglike.cp"),
 					  n.iter=30000, thin=10)
 	 
-	#save model run information	
+	#save Bayesian model run information	
 
 	# extract posterior summary statistics of interest.
 	Mod.out<-summary(s.mod.coda)
@@ -112,7 +112,7 @@ for(i in 1:numsims){
 	write.table(chain3,paste0(wd.out,"\\simulation",i,"chain3_coda.csv"), sep=",")
 			
 
-	#run mcmc plots on key params
+	#create mcmc plots for key params
 			
 mcmcplot(s.mod.coda, parms=c("mu","m","sig.s","sig","mu.nh","sig.nh",
 											  "mu.cp","sig.cp","sumLL.cp","sumLL.nh",
@@ -128,8 +128,8 @@ mcmcplot(s.mod.coda, parms=c("mu","m","sig.s","sig","mu.nh","sig.nh",
 #################################
 #partial pooling indices 
 #for degree of pooling
-
-#HB is hierarchical model
+#define function to calculate PPI
+#HB is the hierarchical model
 #CP is the complete pooling model,
 #NH is the non-hierarchical model 
 Pind<-function(HB,CP,NH){
@@ -138,7 +138,9 @@ Pind<-function(HB,CP,NH){
 
 
 #################################
-# partial pooling with WAIC######
+# partial pooling index based on#
+# with WAIC effective number of #
+# parameters                    #
 #################################
 
 #read in coda from model runs
@@ -148,14 +150,15 @@ CODA1 <- list()
 CODA2 <- list()
 CODA3 <- list()
 CODAALL <- list()
-for(i in 1:numsims){
+for(i in 1:Ndatasets){
 	CODA1[[i]] <- read.csv(paste0(wd.out,"\\simulation",i,"chain1_coda.csv"))
 	CODA2[[i]] <- read.csv(paste0(wd.out,"\\simulation",i,"chain2_coda.csv"))
 	CODA3[[i]] <- read.csv(paste0(wd.out,"\\simulation",i,"chain3_coda.csv"))
 	#join all iterations together
 	CODAALL[[i]] <- rbind(CODA1[[i]],CODA2[[i]],CODA3[[i]])
 }
-#subset to pull log likelihood out of columns
+#subset to pull log likelihood out of columns 
+#(note: the log-likelihood of each observations is computed inside of the JAGS model code, for each model variant and each dataset):
 dexps3 <- "\\.*[[:digit:]]*\\."
 
 loglike.hh <- list()
@@ -163,7 +166,7 @@ loglike.nh <- list()
 loglike.cp <- list()
 
 
-for(i in 1:numsims){
+for(i in 1:Ndatasets){
 	loglike.hh[[i]]<-CODAALL[[i]][,gsub(dexps3,"",colnames(CODAALL[[i]]))=="loglike"]
 	loglike.nh[[i]]<-CODAALL[[i]][,gsub(dexps3,"",colnames(CODAALL[[i]]))=="loglikenh"]
 	loglike.cp[[i]]<-CODAALL[[i]][,gsub(dexps3,"",colnames(CODAALL[[i]]))=="loglikecp"]
@@ -172,12 +175,14 @@ for(i in 1:numsims){
 }
 
 
-#calculate the variation of log likelihood across iterations of the model
+#calculate the posterior variatiances of the log likelihood,
+# but summing the above posterior variances across all observations within each dataseta
+#for each observation across iterations of the model for each dataset
 var.ll.hh <- list()
 var.ll.nh <- list()
 var.ll.cp <- list()
 
-for(i in 1:numsims){
+for(i in 1:Ndatasets){
 	var.ll.hh[[i]] <- apply(loglike.hh[[i]],2,"var")
 	var.ll.nh[[i]] <- apply(loglike.nh[[i]],2,"var")
 	var.ll.cp[[i]] <- apply(loglike.cp[[i]],2,"var")
@@ -188,7 +193,7 @@ for(i in 1:numsims){
 pWAIC.nh <- numeric(0)
 pWAIC.hh <- numeric(0)
 pWAIC.cp <- numeric(0)
-for(i in 1:numsims){
+for(i in 1:Ndatasets){
 	pWAIC.nh[i] <- sum(var.ll.nh[[i]])
 	pWAIC.hh[i] <- sum(var.ll.hh[[i]])
 	pWAIC.cp[i] <- sum(var.ll.cp[[i]])
